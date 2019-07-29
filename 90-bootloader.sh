d1b94d325679c04f20dbe57a33896eb0561a5bf9
Louis Germain <germain.louis.80@gmail.com>
2019-07-29 22:11:03 +0200

    Add bootloader instructions

---
+++ 90-bootloader.sh
@@ -0,0 +1,4 @@
+# bootloader installation
+# grub-install --root-directory=/mnt --target={platform} /dev/sdX
+# arch-chroot /mnt pacman -S grub
+# arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg

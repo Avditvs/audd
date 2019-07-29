15b3f0df9e2d7f3b5d4974290f40c943448f6cd1
Louis Germain <germain.louis.80@gmail.com>
2019-07-29 22:31:45 +0200

    Update 60-locale-conf

--- 60-localeConf.sh
+++ 60-localeConf.sh
@@ -1,7 +1,5 @@
-# need to be ran in chroot environment
-
 # set locale
 echo "Setting locale configuration"
-sed -i '/#fr_FR.UTF-8/s/^#//g' /etc/locale.gen
-locale-gen
-echo LANG="fr_FR.UTF-8" > /etc/locale.conf
+arch-chroot /mnt /bin/bash -c "sed -i '/#fr_FR.UTF-8/s/^#//g' /etc/locale.gen"
+arch-chroot /mnt locale-gen
+arch-chroot /mnt /bin/bash -c 'echo LANG="fr_FR.UTF-8" > /etc/locale.conf'

003ca849a85840c2d139b35edd14794fb6e5e0ba
Louis Germain <germain.louis.80@gmail.com>
2019-08-02 15:29:46 +0200

    Add force option to ln

--- post_install/50-dotfiles.sh
+++ post_install/50-dotfiles.sh
@@ -2,22 +2,25 @@
 git clone http://www.github.com/Avditvs/arch_rice ~/dotfiles
 cd ~
 
+
+
+
 # making symlinks
-ln -s ~/dotfiles/.aliases
-ln -s ~/dotfiles/.bashrc
-ln -s ~/dotfiles/.profile
-ln -s ~/dotfiles/.scripts
-ln -s ~/dotfiles/.Xresources
-ln -s ~/dotfiles/.zshrc
+ln -sf ~/dotfiles/.aliases
+ln -sf ~/dotfiles/.bashrc
+ln -sf ~/dotfiles/.profile
+ln -sf ~/dotfiles/.scripts
+ln -sf ~/dotfiles/.Xresources
+ln -sf ~/dotfiles/.zshrc
 
 # .config symlinks
 cd .config
-ln -s ~/dotfiles/.config/compton
-ln -s ~/dotfiles/.config/dunst
-ln -s ~/dotfiles/.config/i3
-ln -s ~/dotfiles/.config/polybar
-ln -s ~/dotfiles/.config/rofi
-ln -s ~/dotfiles/.config/.wallpaper
+ln -sf ~/dotfiles/.config/compton
+ln -sf ~/dotfiles/.config/dunst
+ln -sf ~/dotfiles/.config/i3
+ln -sf ~/dotfiles/.config/polybar
+ln -sf ~/dotfiles/.config/rofi
+ln -sf ~/dotfiles/.config/.wallpaper
 
 
 
